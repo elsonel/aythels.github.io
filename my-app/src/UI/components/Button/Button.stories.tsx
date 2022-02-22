91e8e9106eaf29484e12d92e2dfbb0242f87b078
Elson <elsonliang@gmail.com>
2022-02-21 19:52:16 -0500

    Simplify story

--- my-app/src/UI/components/Button/Button.stories.tsx
+++ my-app/src/UI/components/Button/Button.stories.tsx
@@ -1,41 +1,44 @@
-import React from 'react';
-import { ComponentStory, ComponentMeta } from '@storybook/react';
-
-import { Button } from './Button';
+import { Meta, Story } from '@storybook/react';
+import { Button, ButtonProps } from './Button';
 
 // More on default export: https://storybook.js.org/docs/react/writing-stories/introduction#default-export
 export default {
   title: 'Example/Button',
   component: Button,
-  // More on argTypes: https://storybook.js.org/docs/react/api/argtypes
-  argTypes: {
-    backgroundColor: { control: 'color' },
+  args: {
+    primary: 'true',
+    size: 'small',
+    label: 'Feature',
   },
-} as ComponentMeta<typeof Button>;
+} as Meta;
 
 // More on component templates: https://storybook.js.org/docs/react/writing-stories/introduction#using-args
-const Template: ComponentStory<typeof Button> = (args) => <Button {...args} />;
+const Template: Story<ButtonProps> = (args) => <Button {...args} />;
 
-export const Primary = Template.bind({});
 // More on args: https://storybook.js.org/docs/react/writing-stories/args
+export const Primary = Template.bind({});
 Primary.args = {
+  ...Primary.args,
   primary: true,
   label: 'Button',
 };
 
 export const Secondary = Template.bind({});
 Secondary.args = {
+  ...Secondary.args,
   label: 'Button',
 };
 
 export const Large = Template.bind({});
 Large.args = {
+  ...Large.args,
   size: 'large',
   label: 'Button',
 };
 
 export const Small = Template.bind({});
 Small.args = {
+  ...Small.args,
   size: 'small',
   label: 'Button',
 };
--- my-app/src/UI/components/Button/Button.tsx
+++ my-app/src/UI/components/Button/Button.tsx
@@ -1,16 +1,11 @@
 import React from 'react';
-import './button.css';
 import styled from "styled-components";
 
-interface ButtonProps {
+export interface ButtonProps extends React.HTMLAttributes<HTMLDivElement>{
   /**
    * Is this the principal call to action on the page?
    */
   primary?: boolean;
-  /**
-   * What background color to use
-   */
-  backgroundColor?: string;
   /**
    * How large should the button be?
    */
@@ -31,7 +26,6 @@ interface ButtonProps {
 export const Button = ({
   primary = false,
   size = 'medium',
-  backgroundColor,
   label,
   ...props
 }: ButtonProps) => {
