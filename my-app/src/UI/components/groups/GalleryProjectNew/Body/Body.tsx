24529bec8320d27d1605dbb94bd2cd5d39d94b46
Elson Liang <elsonliang@gmail.com>
2023-03-02 16:43:59 -0800

    Refactor body page layout

--- my-app/src/UI/components/groups/GalleryProjectNew/Body/Body.tsx
+++ my-app/src/UI/components/groups/GalleryProjectNew/Body/Body.tsx
@@ -20,41 +20,38 @@ export const Body: React.FC<BodyProps> = ({
   return (
     <BodyWrapper {...props}>
       <Title>KINETIC BOARDWALK</Title>
-      <Layout>
-        <TextWrapper>
-          <FactsWrapper>
-            <div>
-              <FactTitle>TYPE</FactTitle>
-              <FactValue>Public Mega-Infastructure</FactValue>
-            </div>
-            <div>
-              <FactTitle>REGION</FactTitle>
-              <FactValue>Los Angeles, California</FactValue>
-            </div>
-            <div>
-              <FactTitle>SUPERVISOR</FactTitle>
-              <FactValue>Nathan Bishop (ARC380)</FactValue>
-            </div>
-            <div>
-              <FactTitle>COLLABORATORS</FactTitle>
-              <FactValue>Elson Liang, Omar</FactValue>
-            </div>
-            <div>
-              <FactTitle>COMPLETED</FactTitle>
-              <FactValue>December 2020</FactValue>
-            </div>
-          </FactsWrapper>
-        </TextWrapper>
-        <TextWrapper>
-          <IntroText>
-            The Kinetic Boardwalk is an adaptive and programmable system of
-            inflating tiles that span the coastline of Venice Beach. Acting as
-            both a public amenity and functional infrastructure, the structure
-            aims to address high tides and rising water levels while
-            substituting as an exhibitive pedestrian walkway.
-          </IntroText>
-        </TextWrapper>
-      </Layout>
+
+      <TopLayout>
+        <IntroText>
+          The Kinetic Boardwalk is an adaptive and programmable system of
+          inflating tiles that span the coastline of Venice Beach. Acting as
+          both a public amenity and functional infrastructure, the structure
+          aims to address high tides and rising water levels while substituting
+          as an exhibitive pedestrian walkway.
+        </IntroText>
+        <FactsWrapper>
+          <Fact>
+            <FactTitle>TYPE</FactTitle>
+            <FactValue>Public Mega-Infastructure</FactValue>
+          </Fact>
+          <Fact>
+            <FactTitle>REGION</FactTitle>
+            <FactValue>Los Angeles, California</FactValue>
+          </Fact>
+          <Fact>
+            <FactTitle>SUPERVISOR</FactTitle>
+            <FactValue>Nathan Bishop (ARC380)</FactValue>
+          </Fact>
+          <Fact>
+            <FactTitle>COLLABORATORS</FactTitle>
+            <FactValue>Elson Liang, Omar</FactValue>
+          </Fact>
+          <Fact>
+            <FactTitle>COMPLETED</FactTitle>
+            <FactValue>December 2020</FactValue>
+          </Fact>
+        </FactsWrapper>
+      </TopLayout>
       <Layout>
         <TextWrapper>
           <ParagraphTitle>
@@ -85,29 +82,28 @@ export const Body: React.FC<BodyProps> = ({
 };
 
 const BodyWrapper = styled.div`
-  position: relative;
-  box-sizing: border-box;
   width: 100%;
-  ${GreaterThan(0, `padding: 30px 20px;`)}
-  ${GreaterThan(500, `padding: 40px 30px;`)}
 `;
 
 const Layout = styled.div`
+  box-sizing: border-box;
   width: min(600px, 100%);
-  display: flex;
-  flex-direction: column;
-
-  width: 100%;
+  margin: auto;
   display: flex;
   flex-direction: column;
   align-items: center;
 
-  ${GreaterThan(0, `gap: 60px;`)}
-  ${GreaterThan(500, `gap: 80px;`)}
+  ${GreaterThan(0, `gap: 80px; padding: 30px 20px;`)}
+  ${GreaterThan(600, `gap: 100px; padding: 40px 30px;`)}
+`;
+
+const TopLayout = styled(Layout)`
+  margin-top: 200px;
+  margin-bottom: 200px;
 `;
 
 const TextWrapper = styled.div`
-  width: min(600px, 100%);
+  width: 100%;
 `;
 
 const Title = styled(Paragraph)`
@@ -119,31 +115,27 @@ const Title = styled(Paragraph)`
 `;
 
 const IntroText = styled(Paragraph)`
-  ${GreaterThan(0, `padding: 100px 0px;`)}
-  ${GreaterThan(500, `padding: 120px 0px;`)}
-
   text-align: center;
   color: ${({ theme }) => theme.color.text};
-  font-weight: ${({ theme }) => theme.font.default.weight.medium};
   ${({ theme }) =>
     GreaterThan(0, `font-size: ${theme.font.default.size.large};`) +
-    GreaterThan(500, `font-size: ${theme.font.default.size.h6};`)}
+    GreaterThan(600, `font-size: ${theme.font.default.size.h6};`)}
 `;
 
 const ParagraphTitle = styled(Paragraph)`
-  padding: 10px 0px;
+  margin-bottom: 10px;
   font-weight: ${({ theme }) => theme.font.default.weight.bold};
   color: ${({ theme }) => theme.color.text};
   ${({ theme }) =>
     GreaterThan(0, `font-size: ${theme.font.default.size.h4};`) +
-    GreaterThan(500, `font-size: ${theme.font.default.size.h3};`)}
+    GreaterThan(600, `font-size: ${theme.font.default.size.h3};`)}
 `;
 
 const ParagraphText = styled(Paragraph)`
   color: ${({ theme }) => theme.color.text};
   ${({ theme }) =>
     GreaterThan(0, `font-size: ${theme.font.default.size.default};`) +
-    GreaterThan(500, `font-size: ${theme.font.default.size.large};`)}
+    GreaterThan(600, `font-size: ${theme.font.default.size.large};`)}
 `;
 
 const FactsWrapper = styled.div`
@@ -152,24 +144,27 @@ const FactsWrapper = styled.div`
   display: flex;
   flex-direction: column;
   ${GreaterThan(0, `gap: 30px;`)}
-  ${GreaterThan(500, `gap: 40px;`)}
+  ${GreaterThan(600, `gap: 40px;`)}
+`;
+
+const Fact = styled.div`
+  width: 100%;
+  display: flex;
+  justify-content: space-between;
+
+  ${GreaterThan(0, `flex-direction: column; align-items: start;`)}
+  ${GreaterThan(600, `flex-direction: row; align-items: center;`)}
 `;
 
 const FactTitle = styled(Paragraph)`
   color: ${({ theme }) => theme.color.text};
   font-weight: ${({ theme }) => theme.font.default.weight.bold};
-  ${({ theme }) =>
-    GreaterThan(0, `font-size: ${theme.font.default.size.default};`) +
-    GreaterThan(500, `font-size: ${theme.font.default.size.large};`)};
   letter-spacing: ${({ theme }) => theme.font.default.letterSpacing.button};
-  margin-bottom: 2px;
 `;
 
 const FactValue = styled(Paragraph)`
   color: ${({ theme }) => theme.color.text};
-  font-size: ${({ theme }) => theme.font.default.size.large};
-
   ${({ theme }) =>
-    GreaterThan(0, `font-size: ${theme.font.default.size.default};`) +
-    GreaterThan(500, `font-size: ${theme.font.default.size.large};`)};
+    GreaterThan(0, `font-size: ${theme.font.default.size.large};`) +
+    GreaterThan(600, `font-size: ${theme.font.default.size.h6};`)};
 `;
