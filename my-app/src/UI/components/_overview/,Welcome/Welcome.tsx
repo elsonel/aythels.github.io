d2eb0fbf02f0d247d67e740ec694993a476b5360
Elson Liang <elsonliang@gmail.com>
2023-08-17 14:17:31 -0700

    Change text

--- my-app/src/UI/components/_overview/,Welcome/Welcome.tsx
+++ my-app/src/UI/components/_overview/,Welcome/Welcome.tsx
@@ -26,9 +26,8 @@ export const Welcome: React.FC<WelcomeProps> = ({
       </Paragraph>
       <br />
       <Paragraph>
-        This is a completely independent project. Each component was hand-coded
-        from scratch and tested against edge cases and responsiveness. Some
-        components may lack proper documentation.
+        This is a completely independent project. Some components may lack
+        proper documentation.
       </Paragraph>
       <br />
       <Paragraph>Feel free to browse around.</Paragraph>
